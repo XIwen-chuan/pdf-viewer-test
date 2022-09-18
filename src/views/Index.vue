a1ec320b83eba36ffb92ed7dbd3eef9acdd1dc7c
Xi Wenchuan <xq1033419145@163.com>
2022-09-18 17:39:39 +0800

    init

---
+++ src/views/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="app">
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.app{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: white;
+}
+</style>
